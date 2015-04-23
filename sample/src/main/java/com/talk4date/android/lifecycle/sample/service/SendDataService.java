99eb4e66f8a731e46c71020e1ae33358d6189878
Florian Gutmann <f.gutmann@redsource.at>
2015-04-23 15:43:21 +0200

    DEV-725 improved documentation

--- sample/src/main/java/com/talk4date/android/lifecycle/sample/service/SendDataService.java
+++ sample/src/main/java/com/talk4date/android/lifecycle/sample/service/SendDataService.java
@@ -9,7 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * A sevice which simulates sending of data to a server.
+ * A service which simulates sending of data to a server.
  */
 public class SendDataService {
 
@@ -29,6 +29,13 @@ public class SendDataService {
 		return instance;
 	}
 
+	/**
+	 * Simulates sending some data to the server.
+	 * Triggers the event receiver once the response from the server came back.
+	 *
+	 * @param data Some data to send to the server. Just used for debug logging in this example.
+	 * @param receiver The receiver to trigger with the response.
+	 */
 	public void sendDataToServer(String data, final EventReceiver<Void> receiver) {
 		// Simulate network latency.
 		log.debug("Sending data to server {}", data);
