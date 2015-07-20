695854f41547e3c7b868a44b0cc0f7f57361c048
Herbert Poul <herbert@poul.at>
2015-07-20 11:09:43 +0200

    event receivers must only be registered once.

--- lifecycle/src/main/java/com/talk4date/android/lifecycle/EventReceivers.java
+++ lifecycle/src/main/java/com/talk4date/android/lifecycle/EventReceivers.java
@@ -1,20 +1,30 @@
 package com.talk4date.android.lifecycle;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * List of event receivers which makes sure destroyed event receivers are removed from the list,
  * and also allows simply posting of events to all contained eventreceivers.
  */
 public class EventReceivers<T> implements EventReceiver.OnDestroyListener<T> {
-	private List<EventReceiver<T>> eventReceivers = new ArrayList<>();
+	private Set<EventReceiver<T>> eventReceivers = new HashSet<>();
 
+	/**
+	 * Registers the given event receiver to be notified of events. If the event receiver was previously registered,
+	 * this method is a no-op.
+	 */
 	public void registerEventReceiver(EventReceiver<T> eventReceiver) {
-		eventReceivers.add(eventReceiver);
-		eventReceiver.addOnDestroyListener(this);
+		if (!eventReceivers.contains(eventReceiver)) {
+			eventReceivers.add(eventReceiver);
+			eventReceiver.addOnDestroyListener(this);
+		}
 	}
 
+	/**
+	 * Removes the given event receiver, so it is no longer notified of events. If the given event receiver
+	 * was not previously registered, this method is a noop.
+	 */
 	public void unregisterEventReceiver(EventReceiver<T> eventReceiver) {
 		eventReceiver.removeOnDestroyListener(this);
 		eventReceivers.remove(eventReceiver);
